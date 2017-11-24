6e234dc489cb391d346d42e788f76ecb48f9dc13
tiayo <656861622@qq.com>
2017-11-24 15:29:41 +0800

    修改user表字段

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,6 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
             $table->string('openid')->unique();
             $table->rememberToken();
             $table->timestamps();
